a3557598eb7543c512ce94ca8e102fde3cdc49ed
Nicklzj <50135771+Nicklzj@users.noreply.github.com>
2023-03-31 18:00:15 +0800

    aa
    aaa

---
+++ 去除空格/去除空格.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include<iostream>
+using namespace std;
+int main()
+{
+
+std::string str = "Hello, world!\n 1111111111 \n 22222 \n333333";
+
+cout << str << endl;
+// 查找换行符的位置
+
+
+// 如果找到了换行符
+size_t pos = str.find('\n');
+while (pos!=-1)
+{
+	if (pos != std::string::npos) {
+		// 在换行符的位置删除一个字符，即删除换行符
+		str.erase(pos, 1);
+	}
+	  pos = str.find('\n');
+}
+
+
+// 输出 "Hello, world!"
+std::cout << str << std::endl;
+return 0;
+}
+
+ 
